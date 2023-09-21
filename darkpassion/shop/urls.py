f96e34ed785aaa6f6742d6b287e24d1fd23476fb
Bernol98 <maksym10@onet.pl>
2023-09-21 20:22:47 +0200

    finally i made images work

--- darkpassion/shop/urls.py
+++ darkpassion/shop/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 app_name = "shop"
 urlpatterns = [
-    path("", views.ProductListView.as_view(),name="shop"),
-    path('images', views.all_products, name='get_products'),
+    path("", views.ProductListView.as_view(), name="product-list"),
+    # path('images', views.display_all_images, name='get_products'),
+    # path('display-images/', views.display_all_images, name='display_all_images')
 ]
